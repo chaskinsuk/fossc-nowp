e47204886bf2d899bbb91313eb0f0b9c2c4b60ca
Chris <chris.haskins@me.com>
2019-01-14 18:42:58 +0000

    More updates

--- public/wp-content/reports/berths.php
+++ public/wp-content/reports/berths.php
@@ -6,22 +6,6 @@ require_once('../../wp/wp-load.php');
 
 // is_user_logged_in() ||  auth_redirect();
 
-// $array = array(
-//     'boat1' => array(
-//         'sail_number' => '12345',
-//         'boat_name' => 'name',
-//         'boat_class' => 'class',
-//         'berth' => 'k28',
-//     ),
-//     'boat2' => array(
-//         'sail_number' => '6789',
-//         'boat_name' => 'name',
-//         'boat_class' => 'class',
-//         'berth' => 'k27',
-//     )
-// );
-// echo serialize( $array );
-// die;
 header("HTTP/1.1 200 OK"); // Forcing the 200 OK header as WP can return 404 otherwise
 
 // Preparing a WP_query
@@ -29,17 +13,6 @@ $return_array = array(); // Initializing the array that will be used for the tab
 
 $args = array(
     'meta_query' => array(
-//         'relation' => 'OR',
-//         array(
-//             'key'     => 'qualifications',
-//             'value'   => 'safetyboat',
-//             'compare' => 'LIKE',
-//         ),
-//         array(
-//             'key'     => 'qualifications',
-//             'value'   => 'Powerboat',
-//             'compare' => 'LIKE',
-//         ),
         array(
             'meta_key' => 'last_name',
             'orderby' => 'meta_value',
--- public/wp-content/reports/pbl2.php
+++ public/wp-content/reports/pbl2.php
@@ -8,16 +8,6 @@ require_once('../../wp/wp-load.php');
 
 header("HTTP/1.1 200 OK"); // Forcing the 200 OK header as WP can return 404 otherwise
 
-// // Preparing a WP_query
-// $the_query = new WP_Query(
-// array(
-// 'post_type' => 'page', // We only want pages
-// // 'post_parent' => 244, // We only want children of a defined post ID
-// 'post_count' => -1 // We do not want to limit the post count
-// // We can define any additional arguments that we need - see Codex for the full list
-// )
-// );
-
 $return_array = array(); // Initializing the array that will be used for the table
 
 $args = array(
@@ -43,20 +33,6 @@ $args = array(
 
 $user_query = new WP_User_Query( $args );
 
-// if ( ! empty( $user_query->get_results() ) ) {
-// 	foreach ( $user_query->get_results() as $user ) {
-        
-//         echo '<p>' . $user->first_name . '</p>';
-//         echo '<p>' . $user->last_name . '</p>';
-//         echo '<p>' . $user->telephone . '</p>';
-//         echo '<p>' . $user->mobile . '</p>';
-//         echo '<p>' . $user->user_email . '</p>';
-//         echo '<p>' . implode(", ", $user->qualifications) . '</p>';
-// 	}
-// } else {
-// 	echo 'No users found.';
-// }
-
 if ( ! empty( $user_query->get_results() ) ) {
     foreach ( $user_query->get_results() as $user ) {
         $return_array[] = array(
@@ -69,27 +45,6 @@ if ( ! empty( $user_query->get_results() ) ) {
         );
     }
 }
-
-// var_dump($results);
-// foreach ($results as $row) {
-//     $return_array[] = array(
-//         'Firstname' => $row->Email);
-// }
-
-// while( $the_query->have_posts() ){
-
-// // Fetch the post
-// $the_query->the_post();
-
-// // Filling in the new array entry
-// $return_array[] = array(
-// 'Id' => get_the_id(), // Set the ID
-// 'Title' => get_the_title(), // Set the title
-// 'Content preview with link' => get_permalink().'||'.strip_tags( strip_shortcodes( substr( get_the_content(), 0, 200 ) ) ).'...'
-// // Get first 200 chars of the content and replace the shortcodes and tags
-// );
-
-// }
 
 // Now the array is prepared, we just need to serialize and output it
 echo serialize( $return_array );
